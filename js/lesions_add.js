23b3c01e2f4eabae88efc1138280fba27c9b5d91
marcma7 <138770711+marcma7@users.noreply.github.com>
2026-08-03 12:51:12 +0200

    Update lesions_add.js

--- js/lesions_add.js
+++ js/lesions_add.js
@@ -1,155 +1,54 @@
 let jugadorActualLesio = null;
 
 
-
 function obrirAfegirLesio(user){
 
     jugadorActualLesio = user;
 
-
-    document
-    .getElementById("nomJugadorLesio")
-    .textContent =
-    capitalize(user.name)
-    +
-    " "
-    +
-    capitalize(user.surname);
-
-
+    document.getElementById("nomJugadorLesio").textContent = capitalize(user.name) + " " + capitalize(user.surname);
 
     const avui = new Date();
-
-
-    document
-    .getElementById("dataLesio")
-    .value =
-    avui.toISOString()
-    .substring(0,10);
-
-
+    document.getElementById("dataLesio").value = avui.toISOString().substring(0,10);
 
     mostrarPantalla("afegirLesio");
-
 }
 
 
-
-
-document
-.getElementById("afegirLesioButton")
-.addEventListener(
-"click",
-guardarLesio
-);
-
+document.getElementById("afegirLesioButton").addEventListener("click", guardarLesio);
 
 
 async function guardarLesio(){
 
-
-    if(!jugadorActualLesio)
-        return;
-
-
+    if(!jugadorActualLesio) return;
 
     const injury = {
-
-
-        user_uuid:
-        jugadorActualLesio.uuid,
-
-
-        team_uuid:
-        document
-        .getElementById("selectorTeams")
-        .value,
-
-
-        data_lesio:
-        document
-        .getElementById("dataLesio")
-        .value
-        .split("-")
-        .reverse()
-        .join("-"),
-
-
-        zona:
-        document
-        .getElementById("zonaLesio")
-        .value,
-
-
-        tipus:
-        document
-        .getElementById("tipusLesio")
-        .value,
-
-
-        gravetat:
-        document
-        .getElementById("gravetatLesio")
-        .value,
-
-
-        demana_fisio:
-        document
-        .getElementById("needsFisio")
-        .checked ? 1 : 0
-
+        user_uuid: jugadorActualLesio.uuid,
+        team_uuid: document.getElementById("selectorTeams").value,
+        data_lesio: document.getElementById("dataLesio").value.split("-").reverse().join("-"),
+        zona: document.getElementById("zonaLesio").value,
+        tipus: document.getElementById("tipusLesio").value,
+        gravetat: document.getElementById("gravetatLesio").value,
+        demana_fisio: document.getElementById("needsFisio").checked ? 1 : 0
     };
 
+    const createdInjury = await insertInjury(injury);
+    
+    if(injury.demana_fisio === 1 && createdInjury.length > 0){
+        const episode = await insertPhysioEpisode({
+            injury_uuid: createdInjury[0].uuid,
+            closed: 0
+        });
 
+        await insertPhysioVisit({
+            episode_uuid: episode[0].uuid,
+            num_visit:1,
+            last_visit:0,
+            visita_feta:0
+        });
+    }
 
-    const createdInjury =
-await insertInjury(injury);
+    mostrarPantalla("teams");
 
-
-
-if(
-    injury.demana_fisio === 1 &&
-    createdInjury.length > 0
-){
-
-    const episode =
-    await insertPhysioEpisode({
-
-        injury_uuid:
-        createdInjury[0].uuid,
-
-        closed: 0
-
-    });
-
-
-
-    await insertPhysioVisit({
-
-        episode_uuid:
-        episode[0].uuid,
-
-        num_visit:1,
-
-        last_visit:0,
-
-        visita_feta:0
-
-    });
-
-}
-
-
-
-mostrarPantalla("teams");
-
-
-    await pickPlayers(
-        document
-        .getElementById("selectorTeams")
-        .value
-    );
-
-
+    await pickPlayers(document.getElementById("selectorTeams").value);
 }
 
