dc452dfc85ea3cd7cf6b2abe1c3890408416d927
marcma7 <138770711+marcma7@users.noreply.github.com>
2026-08-03 13:28:51 +0200

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,33 +1,27 @@
 function guardarUsuariLocal(user) {
-
     localStorage.setItem("code", user.code);
     localStorage.setItem("role", user.role);
     localStorage.setItem("userUuid", user.uuid);
-
 }
 
-function eliminarUsuariLocal() {
 
+function eliminarUsuariLocal() {
     localStorage.removeItem("code");
     localStorage.removeItem("role");
     localStorage.removeItem("userUuid");
-
 }
+
 
 function obtenirCodeLocal() {
-
     return localStorage.getItem("code");
-
 }
+
 
 function obtenirRoleLocal() {
-
     return localStorage.getItem("role");
-
 }
 
+
 function obtenirUserUuidLocal() {
-
     return localStorage.getItem("userUuid");
-
 }
