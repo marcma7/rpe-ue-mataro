7ffc3cdd71aa0f9fd2fcf59b348be4e0b0d8ab06
marcma7 <138770711+marcma7@users.noreply.github.com>
2026-08-03 11:59:38 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,243 +1,114 @@
 const inputCodi = document.getElementById("codi");
 const botoEntrar = document.getElementById("entrarButton");
 
-document
-.getElementById("valoracionsButton")
-.onclick = async ()=>{
-
-    mostrarPantalla(
-        "gestioValoracions"
-    );
-
+document.getElementById("valoracionsButton").onclick = async ()=>{
+    mostrarPantalla("gestioValoracions");
     await loadGestValoracions();
-
 };
 
-document
-.getElementById("aplicarJugadorsButton")
-.addEventListener(
-"click",
-()=>{
-
-
-
-    document
-    .getElementById("botonsSessioFinal")
-    .style.display="block";
-
-    document
-    .getElementById("zonaJugadorsSessio")
-    .style.display="block";
-
-
+document.getElementById("aplicarJugadorsButton").addEventListener("click", ()=>{
+    document.getElementById("botonsSessioFinal").style.display="block";
+    document.getElementById("zonaJugadorsSessio").style.display="block";
 });
 
-
-document
-.getElementById(
-    "tornarValoracionsButton"
-)
-.onclick = () => {
-
-    mostrarPantalla(
-        "gestioValoracions"
-    );
-
+document.getElementById("tornarValoracionsButton").onclick = () => {
+    mostrarPantalla("gestioValoracions");
 };
 
-
-document
-.getElementById(
-    "tornarLesionsButton"
-)
-.onclick = () => {
-
-    mostrarPantalla(
-        "lesions"
-    );
-
+document.getElementById("tornarLesionsButton").onclick = () => {
+    mostrarPantalla("lesions");
 };
 
-document
-    .getElementById("tornarQuestionarisButton")
-    .addEventListener("click", () => {
+document.getElementById("tornarQuestionarisButton").addEventListener("click", () => {
+    mostrarPantalla("gestioQuestionaris");
+});
 
-        mostrarPantalla("gestioQuestionaris");
+document.getElementById("tornarModificarTeamsButton").addEventListener("click", () => {
+    mostrarPantalla("teams");
+});
 
-    });
+document.getElementById("tornarGestioButton").addEventListener("click", () => {
+    mostrarPantalla("management");
+});
 
-document
-    .getElementById("tornarModificarTeamsButton")
-    .addEventListener("click", () => {
-        mostrarPantalla("teams");
-    });
+document.getElementById("tornarEliminarTeamsButton").addEventListener("click", () => {
+    mostrarPantalla("teams");
+});
 
-document
-    .getElementById("tornarGestioButton")
-    .addEventListener("click", () => {
-        mostrarPantalla("management");
-    });
-
-document
-    .getElementById("tornarEliminarTeamsButton")
-    .addEventListener("click", () => {
-        mostrarPantalla("teams");
-    });
-
-document
-.getElementById("fisioButton")
-.addEventListener(
-"click",
-()=>{
-
+document.getElementById("fisioButton").addEventListener("click", ()=>{
     mostrarPantalla("fisio");
-
 });
 
-
-document
-.getElementById("novaPreguntaButton")
-.onclick = () => {
+document.getElementById("novaPreguntaButton").onclick = () => {
     obrirNovaPregunta();
 };
 
-
-document
-.getElementById("questionarisButton")
-.addEventListener(
-"click",
-async ()=>{
-
-    mostrarPantalla(
-        "gestioQuestionaris"
-    );
-
+document.getElementById("questionarisButton").addEventListener("click", async ()=>{
+    mostrarPantalla("gestioQuestionaris");
     await loadGestQuestionaris();
-
 });
 
-
-
-
-document
-.getElementById("enrereLesions")
-.addEventListener(
-"click",
-()=>{
-
+document.getElementById("enrereLesions").addEventListener("click", ()=>{
     mostrarPantalla("teams");
-
 });
 
-const botoSortir =
-    document.getElementById("botoSortir");
-
+const botoSortir = document.getElementById("botoSortir");
 botoSortir.addEventListener("click", sortir);
 
-const sortirButtonRPE =
-    document.getElementById("sortirButtonRPE");
-
+const sortirButtonRPE = document.getElementById("sortirButtonRPE");
 sortirButtonRPE.addEventListener("click", sortir);
 
 botoEntrar.addEventListener("click", entrar);
 
-
-document
-    .getElementById("enrereDades")
-    .addEventListener("click", ()=>{
-
-        mostrarPantalla("management");
-
-    });
+document.getElementById("enrereDades").addEventListener("click", ()=>{
+    mostrarPantalla("management");
+});
 
 window.addEventListener("load", iniciarAplicacio);
 
-const botoConfirmar =
-    document.getElementById("confirmarRPEButton");
+const botoConfirmar = document.getElementById("confirmarRPEButton");
 
 botoConfirmar.addEventListener("click", async () => {
-
     const code = obtenirCodeLocal();
-
     const user = await fetchUserByCode(code);
-
-    if (!user)
-        return;
-
+    if (!user) return;
     await confirmarRPE(user);
-
 });
 
-document
-    .getElementById("confirmarQuestionariButton")
-    .addEventListener("click", async () => {
+document.getElementById("confirmarQuestionariButton").addEventListener("click", async () => {
+    await confirmarQuestionari();
+});
 
-        await confirmarQuestionari();
+document.getElementById("equipsButton").addEventListener("click", async () => {
+    mostrarPantalla("teams");
+    await loadTeams();
+});
 
-    });
+document.getElementById("dadesButton").addEventListener("click", obrirDades);
 
-
-document
-    .getElementById("equipsButton")
-    .addEventListener("click", async () => {
-
-        mostrarPantalla("teams");
-
-        await loadTeams();
-
-    });
-
-document
-.getElementById("dadesButton")
-.addEventListener(
-"click",
-obrirDades
-);
-
-
-document
-.getElementById("passarValoracioButton")
-.addEventListener(
-"click",
-async()=>{
-
+document.getElementById("passarValoracioButton").addEventListener("click", async()=>{
     await obrirSelectorValoracions();
-
 });
 
-document
-    .getElementById("nouEquipButton")
-    .addEventListener("click", crearEquip);
+document.getElementById("nouEquipButton").addEventListener("click", crearEquip);
 
-// ----------------------------
 
 async function iniciarAplicacio() {
-
     const code = obtenirCodeLocal();
-
-    if (!code) {
-        return;
-    }
-
+    if (!code) return;
     inputCodi.value = code;
 
     const user = await fetchUserByCode(code);
 
     if (!user) {
-
         eliminarUsuariLocal();
-
-        alert(
-            "La informació d'aquest usuari ha canviat o ha estat eliminat."
-        );
-
+        alert("La informació d'aquest usuari ha canviat o ha estat eliminat.");
         return;
     }
 
     await decideRoute(user);
 }
 
-// ----------------------------
 
 async function entrar() {
 
@@ -257,53 +128,36 @@ async function entrar() {
     botoEntrar.textContent = "ENTRAR";
 
     if (!user) {
-
-        alert(
-            "Aquest codi no està a la base de dades."
-        );
-
+        alert("Aquest codi no està a la base de dades.");
         return;
     }
 
     guardarUsuariLocal(user);
-
     await decideRoute(user);
 }
 
-// ----------------------------
 
 async function decideRoute(user) {
 
     if (user.role !== "JUGADOR") {
-
         mostrarPantalla("management");
         return;
     }
 
-    const questionaris =
-        await getQuestionarisPerContestar(user.uuid);
+    const questionaris = await getQuestionarisPerContestar(user.uuid);
 
     if (questionaris.length === 0) {
-
-    mostrarPantalla("rpe");
-
-    await loadRPE(user);
-
-    await acabarLoadRPE(user);
-
-} else {
-
-    mostrarPantalla("questionaris");
-
-await loadQuestionarisPendents(user, questionaris);
-
-}
+        mostrarPantalla("rpe");
+        await loadRPE(user);
+        await acabarLoadRPE(user);
+    } else {
+        mostrarPantalla("questionaris");
+        await loadQuestionarisPendents(user, questionaris);
+    }
 }
 
-// ----------------------------
 
 function mostrarPantalla(pantalla) {
-
     document.getElementById("pantallaLogin").style.display = "none";
     document.getElementById("pantallaRPE").style.display = "none";
     document.getElementById("pantallaQuestionaris").style.display = "none";
@@ -314,158 +168,50 @@ function mostrarPantalla(pantalla) {
     document.getElementById("pantallaDeleteSessions").style.display = "none";
     document.getElementById("pantallaDades").style.display = "none";
     document.getElementById("pantallaSeleccionValoracio").style.display = "none";
-document.getElementById("pantallaPassarValoracio").style.display = "none";
-document.getElementById("pantallaFisio").style.display = "none";
-document.getElementById("pantallaLesions").style.display = "none";
-document.getElementById("pantallaAssignarHora").style.display="none";
-document.getElementById("pantallaVisites").style.display="none";
-document.getElementById("pantallaInfoVisita").style.display="none";
-document.getElementById("pantallaAfegirLesio").style.display="none";
-document.getElementById("pantallaGestioQuestionaris").style.display="none";
-document.getElementById("pantallaEnviarQuestionari").style.display="none";
-document.getElementById("questions").style.display = "none";
-document.getElementById("addQuestion").style.display = "none";
-document.getElementById("pantallaGestioValoracions").style.display="none";
-document.getElementById("pantallaValoracioItems").style.display="none";
-document.getElementById("pantallaAddValoracioItem").style.display="none";
+    document.getElementById("pantallaPassarValoracio").style.display = "none";
+    document.getElementById("pantallaFisio").style.display = "none";
+    document.getElementById("pantallaLesions").style.display = "none";
+    document.getElementById("pantallaAssignarHora").style.display="none";
+    document.getElementById("pantallaVisites").style.display="none";
+    document.getElementById("pantallaInfoVisita").style.display="none";
+    document.getElementById("pantallaAfegirLesio").style.display="none";
+    document.getElementById("pantallaGestioQuestionaris").style.display="none";
+    document.getElementById("pantallaEnviarQuestionari").style.display="none";
+    document.getElementById("questions").style.display = "none";
+    document.getElementById("addQuestion").style.display = "none";
+    document.getElementById("pantallaGestioValoracions").style.display="none";
+    document.getElementById("pantallaValoracioItems").style.display="none";
+    document.getElementById("pantallaAddValoracioItem").style.display="none";
 
-
-    if (pantalla === "login") {
-        document.getElementById("pantallaLogin").style.display = "flex";
-    }
-
-    if (pantalla === "rpe") {
-        document.getElementById("pantallaRPE").style.display = "flex";
-    }
-
-    if (pantalla === "questionaris") {
-        document.getElementById("pantallaQuestionaris").style.display = "flex";
-    }
-
-    if (pantalla === "management") {
-        document.getElementById("pantallaManagement").style.display = "flex";
-    }
-
-    if (pantalla === "teams") {
-    document.getElementById("pantallaTeams").style.display = "flex";
+    if (pantalla === "login") document.getElementById("pantallaLogin").style.display = "flex";
+    if (pantalla === "rpe") document.getElementById("pantallaRPE").style.display = "flex";
+    if (pantalla === "questionaris") document.getElementById("pantallaQuestionaris").style.display = "flex";
+    if (pantalla === "management") document.getElementById("pantallaManagement").style.display = "flex";
+    if (pantalla === "teams") document.getElementById("pantallaTeams").style.display = "flex";
+    if (pantalla === "sessions") document.getElementById("pantallaSessions").style.display = "flex";
+    if (pantalla === "modifySessions") document.getElementById("pantallaModifySessions").style.display = "flex";
+    if (pantalla === "deleteSessions") document.getElementById("pantallaDeleteSessions").style.display = "flex";
+    if (pantalla === "dades") document.getElementById("pantallaDades").style.display = "flex";
+    if (pantalla === "seleccionValoracio") document.getElementById("pantallaSeleccionValoracio").style.display = "flex";
+    if (pantalla === "passValoracio") document.getElementById("pantallaPassarValoracio").style.display = "flex";
+    if (pantalla === "fisio") document.getElementById("pantallaFisio").style.display="flex";
+    if (pantalla === "lesions") document.getElementById("pantallaLesions").style.display="flex";
+    if(pantalla==="assignarHora") document.getElementById("pantallaAssignarHora").style.display="flex";
+    if(pantalla==="visites") document.getElementById("pantallaVisites").style.display="flex";
+    if(pantalla==="infoVisita") document.getElementById("pantallaInfoVisita").style.display="flex";
+    if(pantalla==="afegirLesio") document.getElementById("pantallaAfegirLesio").style.display="flex";
+    if(pantalla==="gestioQuestionaris") document.getElementById("pantallaGestioQuestionaris").style.display="flex";
+    if(pantalla==="enviarQuestionari") document.getElementById("pantallaEnviarQuestionari").style.display="flex";
+    if (pantalla === "questions") document.getElementById("questions").style.display = "flex";
+    if (pantalla === "addQuestion") document.getElementById("addQuestion").style.display = "flex";
+    if(pantalla==="gestioValoracions") document.getElementById("pantallaGestioValoracions").style.display="flex";
+    if(pantalla==="valoracioItems") document.getElementById("pantallaValoracioItems").style.display="flex";
+    if(pantalla==="addValoracioItem") document.getElementById("pantallaAddValoracioItem").style.display="flex";
 }
 
-if (pantalla === "sessions") {
-    document.getElementById("pantallaSessions").style.display = "flex";
-}
-
-if (pantalla === "modifySessions") {
-    document.getElementById("pantallaModifySessions").style.display = "flex";
-}
-
-if (pantalla === "deleteSessions") {
-    document.getElementById("pantallaDeleteSessions").style.display = "flex";
-}
-
-if (pantalla === "dades") {
-    document.getElementById("pantallaDades").style.display = "flex";
-}
-
-
-if (pantalla === "seleccionValoracio") {
-    document.getElementById("pantallaSeleccionValoracio").style.display = "flex";
-}
-
-if (pantalla === "passValoracio") {
-    document.getElementById("pantallaPassarValoracio").style.display = "flex";
-}
-
-if (pantalla === "fisio") {
-
-    document
-    .getElementById("pantallaFisio")
-    .style.display="flex";
-
-}
-
-if (pantalla === "lesions") {
-
-    document
-    .getElementById("pantallaLesions")
-    .style.display="flex";
-
-}
-
-if(pantalla==="assignarHora"){
-
-    document
-    .getElementById("pantallaAssignarHora")
-    .style.display="flex";
-
-}
-
-if(pantalla==="visites"){
-
-    document
-    .getElementById("pantallaVisites")
-    .style.display="flex";
-
-}
-
-if(pantalla==="infoVisita"){
-
-    document
-    .getElementById("pantallaInfoVisita")
-    .style.display="flex";
-
-}
-
-if(pantalla==="afegirLesio"){
-
-    document
-    .getElementById("pantallaAfegirLesio")
-    .style.display="flex";
-
-}
-
-if(pantalla==="gestioQuestionaris"){
-
-    document
-    .getElementById("pantallaGestioQuestionaris")
-    .style.display="flex";
-
-}
-
-if(pantalla==="enviarQuestionari"){
-
-    document
-    .getElementById("pantallaEnviarQuestionari")
-    .style.display="flex";
-
-}
-
-if (pantalla === "questions") {
-    document.getElementById("questions").style.display = "flex";
-}
-
-if (pantalla === "addQuestion") {
-    document.getElementById("addQuestion").style.display = "flex";
-}
-
-if(pantalla==="gestioValoracions")
-    document.getElementById("pantallaGestioValoracions").style.display="flex";
-
-if(pantalla==="valoracioItems")
-    document.getElementById("pantallaValoracioItems").style.display="flex";
-
-if(pantalla==="addValoracioItem")
-    document.getElementById("pantallaAddValoracioItem").style.display="flex";
-
-
-}
-
-// ----------------------------
 
 function sortir() {
-
     eliminarUsuariLocal();
-
     inputCodi.value = "";
-
     mostrarPantalla("login");
 }
