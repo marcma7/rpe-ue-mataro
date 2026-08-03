e27483da69905beea180697be60fb2171d05967d
marcma7 <138770711+marcma7@users.noreply.github.com>
2026-08-03 12:09:40 +0200

    Update dialogs.js

--- js/dialogs.js
+++ js/dialogs.js
@@ -1,46 +1,25 @@
 function closeDialog(){
-
     document.getElementById("dialogOverlay").style.display="none";
-
 }
 
 
 function showDialog(title,message,buttons){
-
-    document.getElementById("dialogTitle").textContent =
-        title;
-
-    document.getElementById("dialogMessage").textContent =
-        message;
-
-    const container =
-        document.getElementById("dialogButtons");
+    document.getElementById("dialogTitle").textContent = title;
+    document.getElementById("dialogMessage").textContent = message;
+    const container = document.getElementById("dialogButtons");
 
     container.innerHTML="";
 
-
     buttons.forEach(button=>{
-
-        const b =
-            document.createElement("button");
-
+        const b = document.createElement("button");
         b.textContent = button.text;
-
         b.onclick = ()=>{
-
             closeDialog();
-
-            if(button.action)
-                button.action();
-
+            if(button.action) button.action();
         };
 
         container.appendChild(b);
-
     });
 
-
-    document.getElementById("dialogOverlay").style.display =
-        "flex";
-
+    document.getElementById("dialogOverlay").style.display = "flex";
 }
